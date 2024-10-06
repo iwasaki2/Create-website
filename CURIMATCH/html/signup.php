aefcaeb32f55b67356ea28c66e7b14f3d7299fe3
Harukiti333 <22311021hi@tama.ac.jp>
2024-10-06 21:32:31 +0900

    PHP

--- CURIMATCH/html/signup.php
+++ CURIMATCH/html/signup.php
@@ -1,8 +1,13 @@
 <?php
+// エラーメッセージの表示を有効にする
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
+
 // データベースの接続情報
-$servername = "localhost";
-$username = "your_username"; // あなたのMySQLのユーザー名
-$password = "your_password"; // あなたのMySQLのパスワード
+$servername = "localhost"; // サーバー名
+$username = "root"; // あなたのMySQLのユーザー名
+$password = ""; // あなたのMySQLのパスワード（空白の場合はそのまま）
 $dbname = "curimatch_db"; // 作成したデータベース名
 
 // データベースに接続
@@ -26,9 +31,11 @@ $sql = "INSERT INTO users (username, email, password) VALUES ('$user', '$email',
 
 // データを挿入
 if ($conn->query($sql) === TRUE) {
-    echo "新規登録が完了しました！";
+    // 登録成功時に新しいHTMLファイルを表示
+    header('Location: signup.html'); // signup.htmlにリダイレクト
+    exit; // スクリプトの実行を終了
 } else {
-    echo "エラー: " . $sql . "<br>" . $conn->error;
+    echo "エラー: " . $sql . "<br>" . $conn->error; // エラーメッセージを表示
 }
 
 // 接続を閉じる
