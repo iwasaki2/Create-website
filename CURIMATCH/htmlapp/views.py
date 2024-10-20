d97dc149d12b95712fea2cf2167a108c0b4a3f64
Harukiti333 <22311021hi@tama.ac.jp>
2024-10-20 23:56:10 +0900

    django用新規登録の作成

--- CURIMATCH/htmlapp/views.py
+++ CURIMATCH/htmlapp/views.py
@@ -1,7 +1,9 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth import authenticate, login as auth_login
 from django.contrib import messages
+from .forms import SignUpForm
 
+# 各種ページ表示ビュー
 def index(request):
     return render(request, 'htmlapp/index.html')
 
@@ -17,28 +19,35 @@ def futures(request):
 def information(request):
     return render(request, 'htmlapp/information.html')
 
+def service(request):
+    return render(request, 'htmlapp/service.html')
+
+def upload(request):
+    return render(request, 'htmlapp/upload.html')
+
+# ユーザー認証関連
 def login(request):
     if request.method == "POST":
         login_email = request.POST['login_email']
         login_password = request.POST['login_password']
-
-        # ユーザーを認証する
         user = authenticate(request, username=login_email, password=login_password)
 
         if user is not None:
-            # 認証に成功した場合、ユーザーをログインさせる
-            auth_login(request, user)  # 組み込みlogin関数の別名を使用
-            return redirect('upload')  # 成功後にリダイレクト
+            auth_login(request, user)
+            return redirect('upload')
         else:
             messages.error(request, "メールアドレスまたはパスワードが間違っています")
+    
+    return render(request, 'htmlapp/login.html')
 
-    return render(request, 'htmlapp/login.html')  # GETリクエスト時にはログインページを表示
+# 新規登録処理
+def signup_view(request):
+    if request.method == 'POST':
+        form = SignUpForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('login')  # 登録成功後、ログインページにリダイレクト
+    else:
+        form = SignUpForm()
 
-def service(request):
-    return render(request, 'htmlapp/service.html')
-
-def signup(request):
-    return render(request, 'htmlapp/signup.html')
-
-def upload(request):
-    return render(request, 'htmlapp/upload.html')
+    return render(request, 'htmlapp/signup.html', {'form': form})
